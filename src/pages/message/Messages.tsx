e5b9941f57ea7581b181ee33a62f73b6548ee516
prajun <prajunbudathoki1@gmail.com>
2025-07-03 13:13:38 +0545

    consoling all the emails from firesotre db

--- src/pages/message/Messages.tsx
+++ src/pages/message/Messages.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { useEffect } from "react";
 import Message from "./Message";
+import { collection, onSnapshot } from "firebase/firestore";
+import { db } from "../../firebase";
 
 const Messages = () => {
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, "emails"), (snapshot) => {
+      const allEmails = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      console.log(allEmails);
+    });
+    return () => unsubscribe();
+  }, []);
   return (
     <div>
       <Message />
